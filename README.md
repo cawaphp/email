2a07aa11c4cd9752d7977488db85dfd0e3087f6e
tchiotludo <tchiot.ludo@gmail.com>
2016-04-14 17:28:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Сáша Email
+-----
+
+## Warning
+Be aware that this package is still in heavy developpement.
+Some breaking change will occure. Thank's for your comprehension.
+
+## Features
+* Simple wrapper over Swift_Mailer
+
+## About
+
+### License
+
+Cawa is licensed under the GPL v3 License - see the `LICENSE` file for details
---
+++ src/MailerFactory.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Email;
+
+use Cawa\Core\DI;
+use Cawa\Uri\Uri;
+
+trait MailerFactory
+{
+    /**
+     * @var array
+     */
+    private static $container = [];
+
+    /**
+     * @param string $name
+     *
+     * @return \Swift_Mailer
+     */
+    private static function mailer(string $name = null) : \Swift_Mailer
+    {
+        if ($return = DI::get(__METHOD__)) {
+            return $return;
+        }
+
+        $config = DI::config()->getIfExists('email/' . ($name ?: 'default'));
+        if (!$config) {
+            $transport = \Swift_MailTransport::newInstance();
+            $return = \Swift_Mailer::newInstance($transport);
+        } elseif (is_callable($config)) {
+            $return = $config();
+        } else {
+            $uri = new Uri($config);
+            switch ($uri->getScheme()) {
+                case 'smtp':
+                    $transport = \Swift_SmtpTransport::newInstance($uri->getHost(), $uri->getPort());
+
+                    if ($uri->getUser()) {
+                        $transport->setUsername($uri->getUser());
+                    }
+
+                    if ($uri->getPassword()) {
+                        $transport->setPassword($uri->getPassword());
+                    }
+
+                    if ($uri->getQuery('auth')) {
+                        $transport->setAuthMode($uri->getQuery('auth'));
+                    }
+
+                    if ($uri->getQuery('encryption')) {
+                        $transport->setEncryption($uri->getQuery('encryption'));
+                    }
+                    break;
+
+                default:
+                    throw new \InvalidArgumentException(sprintf("Undefined email mailer type '%s'", $uri->getScheme()));
+                    break;
+            }
+            $return = \Swift_Mailer::newInstance($transport);
+        }
+
+        return DI::set(__METHOD__,  $name, $return);
+    }
+}
